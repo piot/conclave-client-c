5854b1bb4d2ad88e63d96262c832cea1dc82f4e0
Peter Bjorklund <piot@hotmail.com>
2023-11-10 14:12:47 +0100

    chore: add header

--- src/include/conclave-client/network_realizer.h
+++ src/include/conclave-client/network_realizer.h
@@ -1,7 +1,7 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Peter Bjorklund. All rights reserved.
+/*----------------------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/conclave-client-c
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+ *--------------------------------------------------------------------------------------------------------*/
 #ifndef CONCLAVE_CLIENT_NETWORK_REALIZE_H
 #define CONCLAVE_CLIENT_NETWORK_REALIZE_H
 
